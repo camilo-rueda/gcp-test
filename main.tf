f769d71e6e6f5370d3b3e38c898b75d4307dc91f
mmusaev <9410421+mmusaev@users.noreply.github.com>
2024-03-11 14:43:18 -0500

    create a bucket

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "google" {
   project = "marat-musaev-training"
   region  = var.location
 }
+
 #try to create a bucket
 resource "google_storage_bucket" "bucket" {
   name     = var.storage_name
